e7067a455635dbc9b7e08ae5627c9ce3aec74a8a
Xiangdong <straybird23@126.com>
2021-05-11 00:20:17 +0000

    change static_pages to map_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root  'static_pages#home'
-  get   'static_pages/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root  'map_pages#home'
+  get '/sample_map', to: 'map_pages#sample_map'
+  
 end
